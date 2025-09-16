c73c5b646311a974fc941de4c3e12629b6bbd5e6
Mireia <mireiagonzalezz03@gmail.com>
2025-09-16 22:54:18 +0200

    added gravity to player

--- game/Player/player.gd
+++ game/Player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var speed := 300.0
 var viewport_size: Vector2
+var gravity := 15.0
+var max_fall_velocity := 1000.0
 
 func _ready() -> void:
 	viewport_size = get_viewport_rect().size
@@ -10,6 +12,10 @@ func _process(delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
+	velocity.y += gravity
+	if velocity.y > max_fall_velocity:
+		velocity.y = max_fall_velocity
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed 
