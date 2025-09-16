b96d872650d3dc7971f2db207af0355f1d93534b
Mireia <mireiagonzalezz03@gmail.com>
2025-09-16 23:49:35 +0200

    limited camera

--- game/game_camera/game_camera.gd
+++ game/game_camera/game_camera.gd
@@ -1,12 +1,21 @@
 class_name GameCamera extends Camera2D
 
+var viewport_size: Vector2
 var player: Player = null
+var limit_bottom_distance = 420
 
 func _ready() -> void:
+	viewport_size = get_viewport_rect().size
 	global_position.x = get_viewport_rect().size.x / 2
+	
+	limit_bottom = viewport_size.y
+	limit_left = 0
+	limit_right = viewport_size.x
 
 func _process(delta: float) -> void:
-	pass
+	if player:
+		if limit_bottom > player.global_position.y + limit_bottom_distance:
+			limit_bottom = player.global_position.y + limit_bottom_distance
 
 func _physics_process(delta: float) -> void:
 	if player:
